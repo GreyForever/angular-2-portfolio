e36b78e6a60f970f70d8e7f193de8b3e958863f7
Grey <lomik008@yandex.ru>
2017-04-11 15:25:16 +0300

    routing

--- app/app-component/app.routes.js
+++ app/app-component/app.routes.js
@@ -3,12 +3,14 @@ var router_1 = require('@angular/router');
 var home_component_1 = require('../home-component/home.component');
 var projects_component_1 = require('../projects-component/projects.component');
 var about_component_1 = require('../about-component/about.component');
+var contacts_component_1 = require('../contacts-component/contacts.component');
 var error_component_1 = require('../error-component/error.component');
 var routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: home_component_1.HomeComponent },
     { path: 'projects', component: projects_component_1.ProjectsComponent },
     { path: 'about', component: about_component_1.AboutComponent },
+    { path: 'contacts', component: contacts_component_1.ContactsComponent },
     { path: '**', component: error_component_1.ErrorComponent }
 ];
 exports.myRouterProviders = [
--- app/app-component/app.routes.ts
+++ app/app-component/app.routes.ts
@@ -2,6 +2,7 @@ import { provideRouter, RouterConfig }  from '@angular/router';
 import { HomeComponent } from '../home-component/home.component';
 import { ProjectsComponent } from '../projects-component/projects.component';
 import { AboutComponent } from '../about-component/about.component';
+import { ContactsComponent } from '../contacts-component/contacts.component';
 import { ErrorComponent } from '../error-component/error.component';
 
 const routes: RouterConfig = [
@@ -9,6 +10,7 @@ const routes: RouterConfig = [
     { path: 'home', component: HomeComponent },
     { path: 'projects',  component: ProjectsComponent },
     { path: 'about',  component: AboutComponent },
+	{ path: 'contacts',  component: ContactsComponent },
     { path: '**', component: ErrorComponent }
 ];
 
